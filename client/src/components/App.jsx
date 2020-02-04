293d8d14e6a9b1d113aa961007323b6db6e076e1
rolopez23 <rob.lopez@gmail.com>
2020-02-04 12:20:29 -0800

    Updates

--- client/src/components/App.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App = () => (<div>I'm an App!</div>)
-
-export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import App from './components/Bluefin-Mortgage-Service/client/src/mortgageComponents/App.jsx';
+import Similiar from './components/Bluefin-Service-SimilarHomes/client/src/components/App.jsx';
+// window.React2 = require('react');
+// console.log(window.React1 === window.React2);
 
 
-ReactDOM.render(<App />, document.getElementById('app'))
+
+ReactDOM.render(<App listingId={1} />, document.getElementById('mortgage'));
+
+ReactDOM.render(<Similiar />, document.getElementById('similiar'));
+// ReactDOM.render(<Schedule listingId={199099335}/>, document.getElementById('schedule'));

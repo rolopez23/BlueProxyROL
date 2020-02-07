9eb044baf20ba824b9ccea95836363a9edbe3c60
rolopez23 <rob.lopez@gmail.com>
2020-02-07 15:54:15 -0800

    MVP for information

--- server/index.js
+++ server/index.js
@@ -13,10 +13,9 @@ const proxyServer = new proxy.Server;
 
 const MortgageBucket = 'http://13.52.171.152:4003';
 const SimiliarBucket = 'http://13.57.210.79:4004';
+const ScheduleBucket = 'http://52.52.152.21:3002';
 const mainPort = 4000;
-const ScheduleServer = 'http://localhost:3002';
-const MortgageServer = 'http://localhost:4003';
-const SimiliarServer = 'http://localhost:4004';
+
 
 const AWS = 'https://bluefinrol.s3-us-west-1.amazonaws.com';
 
@@ -24,14 +23,13 @@ const AWS = 'https://bluefinrol.s3-us-west-1.amazonaws.com';
 mainApp.use(morgan('combined'));
 
 mainApp.get('/mortgageApp/*', (req, res) => {
-  // console.log('EC2 Request');
-  // // apiProxy.web(req, res, {target: MortgageServer});
-  // proxyServer.redirect(req, res, AWS);
   const fullRoute = MortgageBucket + '/dist/bundle.js';
-  console.log(fullRoute);
   axios.get(fullRoute)
+    .catch((error) => {
+      res.send(error);
+    })
     .then((response)=>{
-      console.log(response.data);
+      // console.log(response.data);
       res.send(response.data);
     });
 });
@@ -40,6 +38,15 @@ mainApp.get('/scheduleTour/*', (req, res) => {
   console.log('AWS Request');
   // apiProxy.web(req, res, {target: MortgageServer});
   proxyServer.redirect(req, res, AWS);
+  const fullRoute = ScheduleBucket + '/bundle.js';
+  axios.get(fullRoute)
+    .catch((error) => {
+      res.send(error);
+    })
+    .then((response)=>{
+      // console.log(response.data);
+      res.send(response.data);
+    });
 });
 
 mainApp.get('/similiarHomes/*', (req, res) => {
@@ -48,6 +55,9 @@ mainApp.get('/similiarHomes/*', (req, res) => {
   // proxyServer.redirect(req, res, AWS);
   const fullRoute = SimiliarBucket + '/dist/bundle.js';
   axios.get(fullRoute)
+    .catch((error) => {
+      res.send(error);
+    })
     .then((response)=>{
       // console.log(response.data);
       res.send(response.data);
@@ -66,15 +76,18 @@ mainApp.get('/mortgage:listingId', (req, res) => {
 });
 
 mainApp.get('/house', (req, res) => {
-  console.log('Schedule Request');
+  // console.log('Schedule Request');
   // apiProxy.web(req, res, {target: ScheduleServer});
-  proxyServer.redirect(req, res, ScheduleServer);
+  console.log('house');
+  proxyServer.redirect(req, res, ScheduleBucket);
+  // axios.get('http://52.52.152.21:3002/house')
+  //   .then((response) => {
+  //     res.send(response.data);
+  //   });
 });
 
 mainApp.get('/similar-listings', (req, res) => {
   console.log('Similar Homes Request');
-  // apiProxy.web(req, res, {target: SimiliarServer});
-  // proxyServer.redirect(req, res, SimiliarServer);
   axios.get('http://13.57.210.79:4004/similar-listings')
     .then((response) => {
       res.send(response.data);
@@ -84,8 +97,3 @@ mainApp.get('/similar-listings', (req, res) => {
 mainApp.listen(mainPort, () => {
   console.log(`Main App is listening on port ${mainPort}, only use to serve up static HTML files.`);
 });
-
-// mainApp.listen(SchedulePort, ()=> {
-
-// });
-
